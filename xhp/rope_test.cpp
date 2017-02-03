556a6b7e60f5596a90f94f53d3893bbc9bad1d1b
Fadhil Mandaga <firodj@gmail.com>
2017-02-03 17:57:05 +0700

    better rope_test

--- xhp/rope_test.cpp
+++ xhp/rope_test.cpp
@@ -7,37 +7,35 @@ int main(int argc, char* argv[])
 {
     code_rope rope = code_rope("x:element-something:wrong");
 
-    cout << "rope = " << rope.c_str() << endl;
-
+    cout << "rope-c_str: " << rope.c_str() << endl;
     rope.xhpLabel();
 
-    cout << "rope = " << rope.c_str() << endl;
-
-    cout << "test = " << (rope == "\\xhp_x__element_something__wrong") << endl;
+    cout << "rope-xhp-label:" << rope.c_str() << endl;
 
     rope = code_rope("   ash   of     hol   ");
     rope.htmlTrim();
-    cout << "[" << rope.c_str() << "]" << endl;
+    cout << "time-everywhere:[" << rope.c_str() << "]" << endl;
 
     rope = code_rope("waht   de    faak");
     rope.htmlTrim();
-    cout << "[" << rope.c_str() << "]" << endl;
+    cout << "trim-middle:[" << rope.c_str() << "]" << endl;
 
     rope = code_rope("");
     rope.htmlTrim();
-    cout << "[" << rope.c_str() << "]" << endl;
+    cout << "trim-empty:[" << rope.c_str() << "]" << endl;
 
     rope = code_rope("                ");
     rope.htmlTrim();
-    cout << "[" << rope.c_str() << "]" << endl;
+    cout << "trim-blank:[" << rope.c_str() << "]" << endl;
+
+    rope = code_rope("'besok ja'n'g'an nakalz'");
+    rope.squote_escape();
+    cout << "squote:" << rope.c_str() << endl;
 
     rope = code_rope("jika uang sa&#64;ya &lt;-25 mak&ku&#x040;&quot;til;a dia we-&amp;  n kamu' 'diam");
-
-    rope.squote_escape();
     rope.xhpDecode();
 
     cout << "decode:" << rope.c_str() << endl;
 
-    cout << endl;
     return 0;
 }
